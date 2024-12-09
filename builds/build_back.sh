01fe41b9a1351fd3296966c9eebbf28d13a8dc56
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-12-09 18:33:36 -0300

    feat(build): added compose file for builds and backend building script

--- builds/build_back.sh
+++ builds/build_back.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-SERVICE=taiga-front
-PROJECT_NAME=tuxditaiga/taiga-front:latest
+SERVICE=taiga-back
+PROJECT_NAME=tuxditaiga/taiga-back:latest
 SAVE_LOCATION=builds
 SERVER_NAME=tuxdi_tools
 SERVER_LOCATION=~/taiga-docker2
 
 
 docker compose -f docker-compose.yml -f build.yml build --no-cache $SERVICE \
-&& docker save -o "$SAVE_LOCATION/$SERVICE.tar" $PROJECT_NAME
+&& docker save -o "$SERVICE.tar" $PROJECT_NAME
 #&& rsync -vzr -e ssh "$SAVE_LOCATION/$SERVICE.tar" $SERVER_NAME:~/images \
 #&& ssh $SERVER_NAME -t "cd $SERVER_LOCATION && docker load -i $SERVICE.tar && docker compose up -d --force-recreate webapp && rm $SERVICE.tar" \
 #&& rm "$SAVE_LOCATION/$SERVICE.tar" \
