f1fc448f188c36a7e32ed1a853d532f27fabe9f7
Franco Carabajal <carabajal.francogabriel@gmail.com>
2024-12-17 11:26:52 -0300

    feat: updated build_front script
    No deploying for now. Just build and send to server

--- builds/build_front.sh
+++ builds/build_front.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+# Build front image, send it to the server and load image into server
+# Cleanup after
 
 SERVICE=taiga-front
-PROJECT_NAME=tuxditaiga/taiga-front:latest
-SAVE_LOCATION=builds
+IMAGE_NAME=tuxditaiga/taiga-front:latest
 SERVER_NAME=tuxdi_tools
-SERVER_LOCATION=~/taiga-docker2
+#SERVER_LOCATION=~/taiga-docker2
 
 
-docker compose -f docker-compose.yml -f build.yml build --no-cache $SERVICE \
-&& docker save -o "$SAVE_LOCATION/$SERVICE.tar" $PROJECT_NAME
-#&& rsync -vzr -e ssh "$SAVE_LOCATION/$SERVICE.tar" $SERVER_NAME:~/images \
-#&& ssh $SERVER_NAME -t "cd $SERVER_LOCATION && docker load -i $SERVICE.tar && docker compose up -d --force-recreate webapp && rm $SERVICE.tar" \
-#&& rm "$SAVE_LOCATION/$SERVICE.tar" \
+docker compose -f ../docker-compose.yml -f ../build.yml build $SERVICE \
+&& docker save -o "$SAVE_LOCATION/$SERVICE.tar" $IMAGE_NAME \
+&& rsync -vzr -e ssh "$SAVE_LOCATION/$SERVICE.tar" $SERVER_NAME:~/images \
+&& ssh $SERVER_NAME -t "cd images && docker load -i $SERVICE.tar && rm $SERVICE.tar" \
+&& rm "$SAVE_LOCATION/$SERVICE.tar"
