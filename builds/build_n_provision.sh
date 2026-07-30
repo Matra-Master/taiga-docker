be87280096dc5e9a69ae1dc0cf360d416d0eaa70
Franco Carabajal <carabajal.francogabriel@gmail.com>
2026-07-30 13:35:08 -0300

    feat(build): updated script so it prunes server images after provision
    Also added useful comments

--- builds/build_n_provision.sh
+++ builds/build_n_provision.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+# Requirements: ssh, docker, rsync, charmbracelet/gum
+# Maintainer: Fran
 # Build front image, send it to the server and load image into server
 # Cleanup after
 SERVER_NAME=tuxdi_tools
@@ -9,14 +11,14 @@ case $SELECTION in
   "Front")
     docker compose -f ../docker-compose.yml -f ../build.yml build taiga-front \
     && docker save -o taiga-front.tar tuxditaiga/taiga-front:latest \
-    && rsync -vzr -e ssh taiga-front.tar "$SERVER_NAME:$SERVER_LOCATION" \
+    && rsync -vzr --progress -e ssh taiga-front.tar "$SERVER_NAME:$SERVER_LOCATION" \
     && ssh $SERVER_NAME -t "cd $SERVER_LOCATION && docker load -i taiga-front.tar && rm taiga-front.tar" \
     && rm taiga-front.tar
     ;;
   "Back")
     docker compose -f ../docker-compose.yml -f ../build.yml build taiga-back \
     && docker save -o taiga-back.tar tuxditaiga/taiga-back:latest \
-    && rsync -vzr -e ssh taiga-back.tar "$SERVER_NAME:$SERVER_LOCATION" \
+    && rsync -vzr --progress -e ssh taiga-back.tar "$SERVER_NAME:$SERVER_LOCATION" \
     && ssh $SERVER_NAME -t "cd $SERVER_LOCATION && docker load -i taiga-back.tar && rm taiga-back.tar" \
     && rm taiga-back.tar \
     ;;
@@ -24,8 +26,10 @@ case $SELECTION in
     docker compose -f ../docker-compose.yml -f ../build.yml build taiga-back taiga-front \
     && docker save -o taiga-back.tar tuxditaiga/taiga-back:latest \
     && docker save -o taiga-front.tar tuxditaiga/taiga-front:latest \
-    && rsync -vzr -e ssh taiga-back.tar taiga-front.tar "$SERVER_NAME:$SERVER_LOCATION" \
+    && rsync -vzr --progress -e ssh taiga-back.tar taiga-front.tar "$SERVER_NAME:$SERVER_LOCATION" \
     && ssh $SERVER_NAME -t "cd $SERVER_LOCATION && docker load -i taiga-back.tar && docker load -i taiga-front.tar && rm taiga-front.tar taiga-back.tar" \
     && rm taiga-back.tar taiga-front.tar
     ;;
 esac
+
+gum confirm "Prune leftover docker images?"&& ssh $SERVER_NAME -t "cd $SERVER_LOCATION && docker image prune" \
